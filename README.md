b1336f9871d589f31cb3e79493e296ef7df0038a
Bruno Marvin <51372744+msscsh@users.noreply.github.com>
2023-04-02 02:11:19 +0000

    Estrutura base do projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+Reste projeto converte TXT em VCF
+
+
+Formato do arquivo de entrada:
+
+NOME |..| CELULAR
---
+++ src/br/com/msscsh/converter/vcf/RunConverter.java
@@ -0,0 +1,11 @@
+package br.com.msscsh.converter.vcf;
+
+public class RunConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println("Preprarando para converter arquivo TXT em VCF");
+
+    }
+
+}

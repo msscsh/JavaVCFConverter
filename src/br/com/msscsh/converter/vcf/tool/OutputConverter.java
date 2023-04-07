449cc521b12fedb808a6c09638f2fa200e19ffc6
Bruno Marvin <51372744+msscsh@users.noreply.github.com>
2023-04-07 01:30:45 +0000

    Adicionando formato utilizado para exportação do VCF

--- src/br/com/msscsh/converter/vcf/tool/OutputConverter.java
+++ src/br/com/msscsh/converter/vcf/tool/OutputConverter.java
@@ -1,5 +1,18 @@
 package br.com.msscsh.converter.vcf.tool;
 
+/**
+ * Classe responsavel pela conversão
+ * 
+ * Formato:
+ * 
+ * BEGIN:VCARD
+ * VERSION:4.0
+ * FN:FullName
+ * N:Suffix
+ * TEL;TYPE=cell:(55) 55555-5555
+ * END:VCARD
+ * 
+ */
 public class OutputConverter {
 
     public static void convertToFile(VCSFile file) {
